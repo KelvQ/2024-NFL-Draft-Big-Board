7e1a25c2d204d5fa400c5bc39b7cdface5d0196e
kquizon <kelvinquizon12327@gmail.com>
2024-03-04 17:53:42 -0800

    Added search function

--- listCreation.js
+++ listCreation.js
@@ -3,13 +3,16 @@ function createPlayerItems(data) {
     var playerList = document.querySelector('.player-list');
     playerList.innerHTML = ""; // Clear previous list items
 
-    data.forEach(function (player, i) {
+    data.forEach(function(player, i) {
+        var originalIndex = playerData.findIndex(function(item) {
+            return item === player;
+        });
+
         var listItem = document.createElement('li');
         listItem.classList.add('player-item');
-        var positionTitle = document.getElementById('position-title');
 
         var html = `
-            <div class="player-rank">${i + 1}</div>
+            <div class="player-rank">${originalIndex + 1}</div>
             <div class="player-image" style="background-image: url('${player.image}')"></div>
             <div class="player-details">
                 <div class="player-name">${player.name}</div>
@@ -25,11 +28,12 @@ function createPlayerItems(data) {
         playerList.appendChild(listItem);
 
         // Add event listener to each list item
-        listItem.addEventListener('click', function () {
+        listItem.addEventListener('click', function() {
             listItem.classList.toggle('expanded');
         });
     });
 }
+
 
 // Call the function initially to create and append list items
 createPlayerItems(playerData);
@@ -66,4 +70,16 @@ document.querySelectorAll('.navbar-list li a').forEach(function (item) {
         });
         this.classList.add('selected'); // Add selected class to clicked item
     });
+});
+
+function filterPlayersBySearch(query) {
+    var filteredData = playerData.filter(function(player) {
+        return player.name.toLowerCase().includes(query.toLowerCase());
+    });
+    createPlayerItems(filteredData);
+}
+
+document.getElementById('searchInput').addEventListener('input', function() {
+    var query = this.value;
+    filterPlayersBySearch(query);
 });
