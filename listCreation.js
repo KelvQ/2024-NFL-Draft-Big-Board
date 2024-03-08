9f119a97b70b65a8f0fac5c25b06eb941bf82a4d
kquizon <kelvinquizon12327@gmail.com>
2024-03-08 11:08:35 -0800

    Moved navbar options under h1 to add room for future features

--- listCreation.js
+++ listCreation.js
@@ -3,8 +3,8 @@ function createPlayerItems(data) {
     var playerList = document.querySelector('.player-list');
     playerList.innerHTML = ""; // Clear previous list items
 
-    data.forEach(function(player, i) {
-        var originalIndex = playerData.findIndex(function(item) {
+    data.forEach(function (player, i) {
+        var originalIndex = playerData.findIndex(function (item) {
             return item === player;
         });
 
@@ -28,12 +28,11 @@ function createPlayerItems(data) {
         playerList.appendChild(listItem);
 
         // Add event listener to each list item
-        listItem.addEventListener('click', function() {
+        listItem.addEventListener('click', function () {
             listItem.classList.toggle('expanded');
         });
     });
 }
-
 
 // Call the function initially to create and append list items
 createPlayerItems(playerData);
@@ -55,17 +54,21 @@ function filterByPosition(position) {
     positionTitle.textContent = position === 'all' ? '2024 Overall NFL Draft Big Board' : '2024 ' + position + ' NFL Draft Big Board';
 
     createPlayerItems(filteredData);
+
+    // Clear search input
+    document.getElementById('searchInput').value = '';
 }
 
-// Add event listeners to navbar items to filter by position and highlight selected item
-document.querySelectorAll('.navbar-list li a').forEach(function (item) {
+
+// Add event listeners to position-selection items to filter by position
+document.querySelectorAll('.position-selection li a').forEach(function (item) {
     item.addEventListener('click', function (e) {
         e.preventDefault();
         var position = this.getAttribute('data-position');
         filterByPosition(position);
 
         // Highlight selected item
-        document.querySelectorAll('.navbar-list li a').forEach(function (item) {
+        document.querySelectorAll('.position-selection li a').forEach(function (item) {
             item.classList.remove('selected'); // Remove selected class from all items
         });
         this.classList.add('selected'); // Add selected class to clicked item
@@ -73,13 +76,13 @@ document.querySelectorAll('.navbar-list li a').forEach(function (item) {
 });
 
 function filterPlayersBySearch(query) {
-    var filteredData = playerData.filter(function(player) {
+    var filteredData = playerData.filter(function (player) {
         return player.name.toLowerCase().includes(query.toLowerCase());
     });
     createPlayerItems(filteredData);
 }
 
-document.getElementById('searchInput').addEventListener('input', function() {
+document.getElementById('searchInput').addEventListener('input', function () {
     var query = this.value;
     filterPlayersBySearch(query);
 });
